342d1a2b218539c1143bc500f9c26b5bfed2f865
devel <vladymyr.danilov@gmail.com>
2015-08-22 02:30:02 +0300

    Create project Menu

---
+++ Menu/Menu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Menu
+//
+//  Created by Danilov on 8/22/15.
+//  Copyright (c) 2015 Workspace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Menu/Menu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Menu
+//
+//  Created by Danilov on 8/22/15.
+//  Copyright (c) 2015 Workspace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
