4fec5c33c7c2039c5d82920c5c721ad7f04670b8
devel <vladymyr.danilov@gmail.com>
2015-08-23 22:43:51 +0300

    Create Property List with array of strings

---
+++ Menu/Menu/MenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuViewController.h
+//  Menu
+//
+//  Created by Danilov on 8/23/15.
+//  Copyright (c) 2015 Workspace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuViewController : UIViewController
+
+@end
